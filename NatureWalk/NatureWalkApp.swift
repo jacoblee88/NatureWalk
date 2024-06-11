24081dd7e73064f92bcb47cf24fb5ae0a6bd5b47
Jacob <jacobleeyo@gmail.com>
2024-06-10 21:18:35 -0400

    Initial Commit

---
+++ NatureWalk/NatureWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureWalkApp.swift
+//  NatureWalk
+//
+//  Created by Jacob Lee on 2024-06-10.
+//
+
+import SwiftUI
+
+@main
+struct NatureWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
