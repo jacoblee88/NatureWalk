5bed044ea096255f6df5cdc0fed975f52f769bda
Jacob <jacobleeyo@gmail.com>
2024-06-12 16:07:26 -0400

    add User DataModel & UserDefaultsManager

---
+++ NatureWalk/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  NatureWalk
+//
+//  Created by Jacob Lee on 2024-06-12.
+//
+
+import Foundation
+
+struct User: Codable {
+    var email: String
+    var password: String
+}
